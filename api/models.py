dc25d2c2c6abbee341281c244a3e682442650744
Dima-F <dfiialo@yahoo.com>
2024-08-16 23:40:18 +0300

    start rest api impl

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from tastypie.resources import ModelResource
+from shop.models import Category, Course
+
+class CategoryResourse(ModelResource):
+    class Meta:
+        queryset = Category.objects.all()
+        resourse_name = 'categories'
+        allowed_methods = ['get']
+
+class CourseResource(ModelResource):
+    class Meta:
+        queryset = Course.objects.all()
+        resourse_name = 'courses'
+        allowed_methods = ['get', 'post', 'delete']
